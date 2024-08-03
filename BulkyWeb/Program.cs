754e25219a6c1260a84f896529620dbaebb8fd09
Jack Li <jack.li@digitalcni.com>
2024-08-03 09:07:08 -0400

    add Identity & extend IdentityUser

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages();
 
 // register ICategoryRepository to the Dependency Injection container 
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
@@ -34,6 +35,7 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
+app.MapRazorPages();
 
 app.MapControllerRoute(
     name: "default",
