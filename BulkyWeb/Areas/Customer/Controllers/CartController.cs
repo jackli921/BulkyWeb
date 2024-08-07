c42acfe920ac565feffa773ad1cf2c9d42f15c3c
Jack Li <jack.li@digitalcni.com>
2024-08-06 20:16:51 -0400

    - add logic to add, subtract, remove item from cart

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -39,6 +39,40 @@ public class CartController : Controller
         return View(ShoppingCartVM);
     }
 
+    public IActionResult Plus(int cartId)
+    {
+        var cartFromDb = _unitOfWork.ShoppingCart.Get(x => x.Id == cartId);
+        cartFromDb.Count += 1;
+        _unitOfWork.ShoppingCart.Update(cartFromDb);
+        _unitOfWork.Save();
+        return RedirectToAction(nameof(Index));
+    }
+    
+    public IActionResult Minus(int cartId)
+    {
+        var cartFromDb = _unitOfWork.ShoppingCart.Get(x => x.Id == cartId);
+        if (cartFromDb.Count <= 1)
+        {
+            // remove from cart
+            _unitOfWork.ShoppingCart.Remove(cartFromDb);
+        }
+        else
+        {
+            cartFromDb.Count -= 1;
+            _unitOfWork.ShoppingCart.Update(cartFromDb);
+        }
+        _unitOfWork.Save();
+        return RedirectToAction(nameof(Index));
+    }
+    
+    public IActionResult Remove(int cartId)
+    {
+        var cartFromDb = _unitOfWork.ShoppingCart.Get(x => x.Id == cartId);
+        _unitOfWork.ShoppingCart.Remove(cartFromDb);
+        _unitOfWork.Save();
+        return RedirectToAction(nameof(Index));
+    }
+
     private double GetPriceBasedOnQuantity(ShoppingCart shoppingCart)
     {
         if (shoppingCart.Count <= 50)
