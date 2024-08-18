b42695c35a33639ce3c8e80009d8af410be630a5
Jack Li <jack.li@digitalcni.com>
2024-08-18 18:43:45 -0400

    add logic to clear user shopping cart session after successful payment redirect

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -180,6 +180,7 @@ public class CartController : Controller
                 _unitOfWork.OrderHeader.UpdateStatus(id, StaticDetails.StatusApproved, StaticDetails.PaymentStatusApproved);
                 _unitOfWork.Save();
             }
+            HttpContext.Session.Clear();
         }
 
         List<ShoppingCart> shoppingCarts = _unitOfWork.ShoppingCart
