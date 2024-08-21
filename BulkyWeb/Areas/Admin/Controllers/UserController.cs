95a2c5a610c822780fc02ff5642122312217bd77
Jack Li <jack.li@digitalcni.com>
2024-08-21 08:30:59 -0400

    add user controller, user view, user.js to display user list page

---
+++ BulkyWeb/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using Bulk.DataAccess.Data;
+using Bulk.DataAccess.Repository;
+using Bulky.Models;
+using Bulky.Models.ViewModel;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Authorize(Roles = StaticDetails.Role_Admin)]
+public class UserController: Controller
+{
+       private readonly ApplicationDbContext _db;
+
+       public UserController(ApplicationDbContext db)
+       {
+              _db = db;
+       }
+       
+       // GET
+       public IActionResult Index()
+       {
+              return View();
+       }
+       
+   
+       #region API Endpoints
+
+       [HttpGet]
+       public IActionResult GetAll()
+       {
+              List<ApplicationUser> objUserList = _db.ApplicationUsers.Include(u => u.Company).ToList();
+              return Json(new { data = objUserList });
+       }
+
+       [HttpDelete]
+       public IActionResult Delete(int id)
+       {
+              return Json(new { success = true, message = "Delete Successful" });
+       }
+       #endregion
+}
---
+++ BulkyWeb/wwwroot/js/user.js
@@ -0,0 +1,23 @@
+﻿var datatable;
+$(document).ready(function() {
+    datatable = $('#tblData').DataTable({
+        ajax: {
+            url: '/admin/user/getall',
+        },
+        columns: [
+            { data: 'name' },
+            { data: 'email' },
+            { data: 'phoneNumber' },
+            { data: 'company.name' },
+            { data: '' },
+            {
+                data: 'id',
+                render: function(data) {
+                    return `<div class="w-75 btn-group" role="group">
+                        <a href="/admin/company/upsert?id=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i> Edit</a>
+                    </div>`
+                }
+            }
+        ]
+    });
+});
